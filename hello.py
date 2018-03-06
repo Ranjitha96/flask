3539c82d77f858cf78413ab0a568e75fe853b9ef
Ranjitha96 <ranjitha.ramesh10@gmail.com>
2018-03-06 16:36:24 +0530

    using template

--- hello.py
+++ hello.py
@@ -1,20 +1,7 @@
-from flask import Flask, url_for
+from flask import Flask
+from flask import render_template
 app=Flask(__name__)
-@app.route('/')
-def index():
-	return 'index'
-
-@app.route('/login')
-def login():
-	return 'login'
-
-@app.route('/user/<username>')
-def profile(username):
-	return '{}\'s profile'.format(username)
-
-with app.test_request_context():
-	print(url_for('index'))
-	print(url_for('login'))
-	print(url_for('login',next='/'))
-	print(url_for('profile',username='john'))
-
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('hello.html',name=name)
