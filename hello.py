fe924ca81e32d087fe9cbb6087fa1d75d5168cc7
Ranjitha96 <ranjitha.ramesh10@gmail.com>
2018-03-07 14:45:25 +0530

    response object

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import request
 from flask import render_template
+from flask import Flask
 app=Flask(__name__)
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-	return render_template('hello.html',name=name)
+@app.route('/login',methods=['POST','GET'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if valid_login(request.form['username'],request.form['password']):
+			return log_the_user_in(request.form['username'])
+		else:
+			error = 'Invalid username/password'
+	return render_template('login.html',error=error)
