dc1505c055bcf3f45bd715d87c3c48a8c7fd804b
Ranjitha96 <ranjitha.ramesh10@gmail.com>
2018-03-07 15:47:34 +0530

    trying out session

--- hello.py
+++ hello.py
@@ -1,13 +1,25 @@
-from flask import request
-from flask import render_template
-from flask import Flask
-app=Flask(__name__)
-@app.route('/login',methods=['POST','GET'])
+from flask import Flask,session,redirect,url_for,escape,request
+app= Flask(__name__)
+app.secret_key= b'ee\n\xbb\x81\xf2\xf5\x96s$D\x7fZ\r\xeaL'
+app.route('/')
+def index():
+	if 'username' in session:
+		return 'Logged in as %s' % escape(session['username'])
+	else:
+		return 'you are not logged in '
+
+@app.route('/login',methods=['GET','POST'])
 def login():
-	error = None
 	if request.method == 'POST':
-		if valid_login(request.form['username'],request.form['password']):
-			return log_the_user_in(request.form['username'])
-		else:
-			error = 'Invalid username/password'
-	return render_template('login.html',error=error)
+		session['username'] = request.form['username']
+		return redirect(url_for('index'))
+	return '''
+		<form method="post">
+			<p><input type=text name=username>
+			<p><input type=submit value=login>
+		</form>
+	'''
+@app.route('/logout')
+def logout():
+	session.pop('username',None)
+	return redirect(url_for('index'))
